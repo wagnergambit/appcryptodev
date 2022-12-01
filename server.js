43d89daf0904e5cbc5be9f64265dd40a79ed1200
slade <wagnergambit@gmail.com>
2022-12-01 01:25:42 -0300

    teste 2 api

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const router = express.Router();
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "API crypto",
-        version: "0.0.1"
+        version: "0.0.2"
     });
 });
 
